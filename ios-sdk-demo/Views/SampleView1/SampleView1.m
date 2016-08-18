486d13579b758990e00ad063a4612d41e0e08b1e
DaidoujiChen <daidoujichen@gmail.com>
2016-08-18 09:39:37 +0800

    add demo for cta button

--- ios-sdk-demo/Views/SampleView1/SampleView1.m
+++ ios-sdk-demo/Views/SampleView1/SampleView1.m
@@ -13,6 +13,9 @@
 @property (weak, nonatomic) IBOutlet UIView *videoView;
 @property (weak, nonatomic) IBOutlet UILabel *titleLabel;
 @property (weak, nonatomic) IBOutlet UIImageView *mainImageView;
+@property (weak, nonatomic) IBOutlet UIButton *ctaButton;
+
+@property (nonatomic, strong) UILabel *ctaLabel;
 
 @end
 
@@ -34,6 +37,37 @@
 
 - (UIImageView *)nativeMainImageView {
     return self.mainImageView;
+}
+
+- (UILabel *)nativeCallToActionTextLabel {
+    return self.ctaLabel;
+}
+
+- (NSArray *)clickableViews {
+    return @[ self.ctaButton ];
+}
+
+#pragma mark - KVO
+
+- (void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary<NSString *,id> *)change context:(void *)context {
+    if ([keyPath isEqualToString:@"ctaLabel.text"]) {
+        [self.ctaButton setTitle:change[@"new"] forState:UIControlStateNormal];
+    }
+}
+
+#pragma mark - Life Cycle
+
+- (id)initWithCoder:(NSCoder *)aDecoder {
+    self = [super initWithCoder:aDecoder];
+    if (self) {
+        self.ctaLabel = [UILabel new];
+        [self addObserver:self forKeyPath:@"ctaLabel.text" options:NSKeyValueObservingOptionNew context:nil];
+    }
+    return self;
+}
+
+- (void)dealloc {
+    [self removeObserver:self forKeyPath:@"ctaLabel.text"];
 }
 
 @end
